8a20838ca2d9e9626aabacc57bcc7aeaa3b6ed43
zmar234 <zachmarino234@gmail.com>
2024-08-14 13:12:22 -0400

    remove unused imports

--- app/src/pages/02_All_Job_Entries.py
+++ app/src/pages/02_All_Job_Entries.py
@@ -3,9 +3,6 @@ logger = logging.getLogger(__name__)
 import requests
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-import pandas as pd
-import pydeck as pdk
-from urllib.error import URLError
 from modules.nav import SideBarLinks
 
 SideBarLinks()
