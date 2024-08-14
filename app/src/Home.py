e44688b61d8858b7435d7c0e2ea1caa5c04cc012
zmar234 <zachmarino234@gmail.com>
2024-08-13 23:34:54 -0400

    home page edits & role edits

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,38 +34,38 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('The Profs App')
+st.title('TalentTrace')
 st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.write('### Welcome to TalentTrace! Please log in.')
 
 # For each of the user personas for which we are implementing
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as John, a Political Strategy Advisor", 
+if st.button("Act as Barney, a Newly Graduated MBA Candidate from Northeastern University", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'applicant'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'Barney'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
     logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    st.switch_page('pages/00_Applicant_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
+if st.button('Act as Sam, Head of the Quantitative Analytics Department of XYZ, a Financial Services Company', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
+    st.session_state['role'] = 'department_head'
+    st.session_state['first_name'] = 'Sam'
     st.switch_page('pages/10_USAID_Worker_Home.py')
 
-if st.button('Act as System Administrator', 
+if st.button('Act as Alex, Applications Analyst at TalentTrace and System Administrator', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
