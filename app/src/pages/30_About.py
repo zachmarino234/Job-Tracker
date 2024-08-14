7c8a51f6417e38adeb086a702b858b5a8598107c
zmar234 <zachmarino234@gmail.com>
2024-08-13 21:54:45 -0400

    about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,11 +8,17 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
-
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
+    As college students who are actively seeking internships, 
+    we find it extremely difficult to keep up with the fast-paced 
+    and convoluted recruiting process. Applications for companies 
+    release at different times, on different websites, with different 
+    interview schedules. This sheer amount of information often makes 
+    a spreadsheet overwhelming and inefficient when it comes to 
+    strategizing our interview process. As well, we have no frame of 
+    reference when it comes to what types of jobs are popular, what 
+    companies are seeing an uptick of applications, etc. With that in 
+    mind, we created TalentTrace, a Job Tracker and database where 
+    users can not only track their job applications, but the entries 
+    they add are aggregated into trends that everyone can see and use.
     """
         )
