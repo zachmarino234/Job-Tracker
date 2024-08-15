44fd45d50c3348ce1f58250d582cf318a7690d26
zmar234 <zachmarino234@gmail.com>
2024-08-15 17:36:07 -0400

    moved the average salary route

--- api/backend/aggData/aggregate_data_routes.py
+++ api/backend/aggData/aggregate_data_routes.py
@@ -24,3 +24,22 @@ def get_most_popular_industries():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# Return industry and average salary
+@aggregate.route('/industry_salary', methods=['GET'])
+def get_industry_salary():
+    current_app.logger.info('GET industry_salary route')
+    cursor=db.get_db().cursor()
+    the_query = '''
+        SELECT name, ROUND(AVG(salary),0) AS Average_Salary 
+        FROM jobRecords
+        NATURAL JOIN industry
+        GROUP BY name
+    '''
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    theResponse=make_response(theData)
+    theResponse.status_code = 200
+    theResponse.mimetype = 'application/json'
+    return theResponse
