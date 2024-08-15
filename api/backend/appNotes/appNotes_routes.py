0da1da02cbd5ad3eac929bb178b5b2200cfabb75
baccaroni <pham.b@northeastern.edu>
2024-08-14 20:54:03 -0400

    adding routes for notes and recruiters

---
+++ api/backend/appNotes/appNotes_routes.py
@@ -0,0 +1,53 @@
+########################################################
+# Notes blueprint of applicantNotes endpoints
+########################################################
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+appNotes = Blueprint('appNotes', __name__)
+
+# Get notes from a particular applicant
+@appNotes.route('/appNotes/<appID>', methods=['GET'])
+def get_app_notes(appID):
+    current_app.logger.info('GET /appNotes/<appID> route')
+    cursor = db.get.db().cursor()
+    query = 'select * from applicantIntNotes where appID={0}'.format(appID)
+    cursor.execute(query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Update content of a particular note for an applicant
+@appNotes.route('/appNotes/<appID>', methods=['PUT'])
+def update_app_notes(appID):
+    current_app.logger.info('PUT /appNotes route')
+    app_info = request.json
+    content = content['content']
+    app_id = app_id['appID']
+    interview_ID = interview_ID['interviewID']
+    query = 'update applicantIntNotes set content = %s where appID = {0}'.format(appID)
+    data = (content, app_id, interview_ID)
+    cursor = db.get_db().cursor()
+    r=cursor.execute(query, data)
+    db.get_db().commit()
+    return 'note updated!'
+
+
+# Remove a note record from the from an applicant
+@appNotes.route('/appNotes/<appID>', methods=['DELETE'])
+def remove_app_notes(appID):
+    current_app.logger.info('DELETE /appNotes route')
+    appNotes_info = request.json
+    content = appNotes_info['content']
+    app_id = appNotes_info['appID']
+    interview_ID = appNotes_info['interviewID']
+    query = 'delete from applicantIntNotes where appID = {0}'.format(appID)
+    data = (content, app_id, interview_ID)
+    cursor = db.get_db().cursor()
+    r=cursor.execute(query, data)
+    db.get_db().commit()
+    return 'note updated!'
---
+++ api/backend/recruiterNotes/recruiterNotes_routes.py
@@ -0,0 +1,64 @@
+########################################################
+# Sample recruiterNotes blueprint of endpoints
+########################################################
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+recruiterNotes = Blueprint('recruiterNotes', __name__)
+
+# Access all notes made by a certain recruiter
+@recruiterNotes.route('/recruiterNotes/<recruiterID>', methods=['GET'])
+def get_recruiterNotes(recruiterID):
+    current_app.logger.info('recruiterNotes.routes.py: GET /recruiterNotes')
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from recruiterIntNotes where recruiterID = {0}'.format(recruiterID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+        the_response = make_response(jsonify(json_data))
+        the_response.status_code = 200
+        the_response.mimetype ='application/json'
+        return the_response
+    
+# Updating notes from a certain recruiter
+@recruiterNotes.route('/recruiterNotes/<recruiterID>', methods=['PUT'])
+def update_recruiterNotes(recruiterID):
+    current_app.logger.info('PUT /recruiterNotes route')
+    recruiterNotes_info = request.json
+    recruiter_ID = recruiterNotes_info['recruiterID']
+    interview_ID = recruiterNotes_info['interviewID']
+    certificates = recruiterNotes_info['PopularCertificates']
+    skill = recruiterNotes_info['PopularSkill']
+    role = recruiterNotes_info['Role']
+    compensation = recruiterNotes_info['compensation_range']
+
+    query = 'update recruiterIntNotes set interviewID = %s, PopularCertificates = %s, PopularSkill = %s, Role = %s, compensation_range = %s where recruiterID = %s'.format(recruiterID)
+    data = (interview_ID, certificates, skill, role, compensation, recruiter_ID)
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'note updated!'
+
+# Removing irrelevant recruiterNote
+@recruiterNotes.route('/recruiterNotes/<recruiterID>', methods=['DELETE'])
+def remove_recruiterNotes(recruiterID):
+    current_app.logger.info('DELETE /recruiterNotes route')
+    recruiterNotes_info = request.json
+    recruiter_ID = recruiterNotes_info['recruiterID']
+    interview_ID = recruiterNotes_info['interviewID']
+    certificates = recruiterNotes_info['PopularCertificates']
+    skill = recruiterNotes_info['PopularSkill']
+    role = recruiterNotes_info['Role']
+    compensation = recruiterNotes_info['compensation_range']
+
+    query = 'delete from recruiterIntNotes where recruiterID = {0}'.format(recruiterID)
+    data = (interview_ID, certificates, skill, role, compensation, recruiter_ID)
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'note deleted!'
+    
